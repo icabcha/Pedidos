8b978e5122077484b9592ae8977b2b39a2edb6d5
JMAG <jgedags341@g.educaand.es>
2024-01-15 17:47:08 +0100

    merger to ivan

--- categoria.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(isset($_SESSION['usuario']) && isset($_SESSION['pass'])){
-       
-        echo "<h1>hola mundo</h1>";
-
-    }
-?>
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<?php
+    require 'funtcion.php';
+    CrearBaseDatos();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedidos</title>
+
 </head>
 <body>
     
